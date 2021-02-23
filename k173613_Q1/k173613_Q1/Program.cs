23a01c449cf54b8e2d9aa9242d59c5b59c164035
Sameer Khowaja - SK <54762751+SameerKhowaja@users.noreply.github.com>
2021-02-23 15:24:53 +0500

    console based captured data from PSX and saved to D drive

---
+++ k173613_Q1/k173613_Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+
+namespace k173613_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.psx.com.pk/market-summary/";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+            Console.WriteLine(html.Result);
+
+            int day = DateTime.Today.Day;
+            int month = DateTime.Today.Month;
+            int year = DateTime.Today.Year;
+
+            DateTime date = new DateTime(year, month, day);
+            string file_name = @"D:\Summary" + date.ToString("dd") + date.ToString("MMM") + date.ToString("yy") + ".html";
+
+            string content = html.Result;
+            File.WriteAllText(file_name, content);
+            Console.WriteLine(file_name + "  Saved to D: drive");
+
+            Console.ReadLine();
+        }
+    }
+}
